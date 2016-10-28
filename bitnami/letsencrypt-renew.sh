5a69d0ccdd2fa90e5750401bc1dbd247007d9421
Alfred Dagenais <adwebmaster@gmail.com>
2016-10-28 18:37:38 -0400

    ADD: new Bitami helpers

---
+++ bitnami/letsencrypt-renew.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Renew Let's Encrypt SSL cert
+
+cd /root/certbot-master/
+./letsencrypt-auto --config /etc/letsencrypt/cli.ini -d domain.com -d www.domain.com certonly
+
+if [ $? -ne 0 ]
+ then
+	ERRORLOG=`tail /var/log/letsencrypt/letsencrypt.log`
+	echo -e "The Lets Encrypt Cert has not been renewed! \n \n" $ERRORLOG | mail -s "Lets Encrypt Cert Alert" postmaster@yourdomain.com
+ else
+	/opt/bitnami/ctlscript.sh stop
+fi
+
+exit 0
